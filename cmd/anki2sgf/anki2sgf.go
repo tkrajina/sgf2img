bddf3c6d8ce2754c8dde9c25067bf58e5905423c
Tomo Krajina <tkrajina@gmail.com>
2022-04-11 08:45:54 +0200

    tmp

--- cmd/anki2sgf/anki2sgf.go
+++ cmd/anki2sgf/anki2sgf.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/rooklift/sgf"
+	"github.com/tkrajina/sgf2img/sgfutils"
 )
 
 const board = `...................
@@ -54,24 +58,88 @@ LB:rn:A,rp:1
 --
 2:...w...B..b.w.bwb..`
 
-func main() {
-	root := sgf.NewTree(19)
-	root.Board().AddStone(sgf.Point(0, 0), sgf.BLACK)
+var (
+	diffLine = regexp.MustCompile(`^\d+:[\.wbWB]+`)
+	tagLine  = regexp.MustCompile(`^\w+:[\.wbWB]+`)
+)
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
-func splitMoves(txt string) (board string, moves [][]string) {
+func main() {
+	node := sgf.NewTree(19)
+	node.Board().AddStone(sgf.Point(0, 0), sgf.BLACK)
+
+	var err error
+	_ = err
 	var initialBoard []string
-	for _, line := strings.Split(txt, "\n") {
+	moves := splitMoves(board)
+	for n, move := range moves {
+		fmt.Println("Move:\n" + strings.Join(move, "\n") + "\n")
+		for _, line := range move {
+			lineLower := strings.ToLower(line)
+			if n == 0 && (lineLower[0] == '.' || lineLower[0] == 'b' || lineLower[0] == 'w') {
+				fmt.Println("initial:", line)
+				initialBoard = append(initialBoard, line)
+			} else if n > 0 && diffLine.MatchString(line) {
+				parts := strings.Split(line, ":")
+				lineNo, err := strconv.ParseInt(parts[0], 10, 16)
+				_ = lineNo
+				panicIfErr(err)
+				whiteColumn := strings.IndexRune(parts[1], 'W') // TODO
+				blackColumn := strings.IndexRune(parts[1], 'B') // TODO
+				if whiteColumn >= 0 {
+					//node, err = node.PlayColour(sgf.Point(whiteColumn, int(lineNo)), sgf.WHITE)
+					//panicIfErr(err)
+				} else if blackColumn >= 0 {
+					//node, err = node.PlayColour(sgf.Point(blackColumn, int(lineNo)), sgf.BLACK)
+					//panicIfErr(err)
+				}
+			} else if tagLine.MatchString(line) {
+				parts := strings.SplitN(line, ":", 2)
+				if len(parts) == 2 {
+					node.AddValue(parts[0], parts[1])
+				}
+			}
+		}
+		if n == 0 {
+			for lineNo, line := range initialBoard {
+				for columNo, rune := range []rune(line) {
+					coords := sgf.Point(columNo, lineNo)
+					switch rune {
+					case 'b':
+						node.AddValue(sgfutils.SGFTagAddBlack, coords)
+					case 'B':
+						_, err := node.PlayColour(coords, sgf.BLACK)
+						panicIfErr(err)
+					case 'w':
+						node.AddValue(sgfutils.SGFTagAddWhite, coords)
+					case 'W':
+						_, err := node.PlayColour(coords, sgf.WHITE)
+						panicIfErr(err)
+					}
+				}
+			}
+		}
+	}
+
+	node.Save("tmp.sgf")
+}
+
+func splitMoves(txt string) (moves [][]string) {
+	for _, line := range strings.Split(txt, "\n") {
+		line = strings.TrimSpace(line)
 		if len(moves) == 0 {
 			moves = append(moves, []string{})
 		}
-		line = strings.TrimSpace(line)
-		lineLower := strings.ToLower(line)
-		if len(line) == 0 {
-			continue
-		} else if strings.HasPrefix(line, "--") {
-		} else if lineLower[0] == "b" || lineLower[0] == "w" || lineLower[0] == "." {
-		} else {
+		if strings.HasPrefix(line, "--") {
+			moves = append(moves, []string{})
+		} else if line != "" {
+			moves[len(moves)-1] = append(moves[len(moves)-1], line)
 		}
 	}
+	return
 }
